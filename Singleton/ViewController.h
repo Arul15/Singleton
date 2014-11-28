e956a68d2f46c61fd695e022086fcb27230ebb01
Arul15 <arulpandiyan@Sybrants-Mac-mini-2.local>
2014-11-28 12:18:27 +0530

    Initial Commit

---
+++ Singleton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Singleton
+//
+//  Created by Arulpandiyan on 28/11/14.
+//  Copyright (c) 2014 Sybrant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
